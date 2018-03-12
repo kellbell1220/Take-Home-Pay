b4878d2bce32caff834e043475fdf84ba8514f29
Kelli Stasiak <30395794+kellbell1220@users.noreply.github.com>
2018-03-12 15:36:24 -0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using static System.Console;
+
+
+namespace TakeHomePay
+{
+    class Employee
+    {
+        private String firstName;
+        private String lastName;
+        private String employeeID;
+        private String payStatus;
+        private Double pay;
+        public Double hours;
+        private Double payRate;
+        public Double FEDERAL = .18;
+        public Double SOCIAL_SECURITY = .06;
+        public Double RETIREMENT = .10;
+
+
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(String firstName, String lastName, String employeeID, String payStatus, Double pay)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.employeeID = employeeID;
+            this.payStatus = payStatus;
+            this.pay = pay;
+
+        }
+
+        public Employee(String firstName, String lastName, String employeeID, String payStatus, Double hours, Double payRate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.employeeID = employeeID;
+            this.payStatus = payStatus;
+            this.hours = hours;
+            this.payRate = payRate;
+        }
+
+        //Getter and Setters for all the private variables
+        public String FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public String LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public String EmployeeID
+        {
+            get
+            {
+                return employeeID;
+            }
+            set
+            {
+                employeeID = value;
+            }
+
+        }
+
+        public String PayStatus
+        {
+            get
+            {
+                return payStatus;
+            }
+            set
+            {
+                payStatus = value;
+            }
+
+        }
+
+        public Double Pay
+        {
+            get
+            {
+                return pay;
+            }
+            set
+            {
+                pay = value;
+            }
+
+        }
+
+        public Double PayRate
+        {
+            get
+            {
+                return payRate;
+            }
+            set
+            {
+                payRate = value;
+            }
+
+        }
+
+        public String ReturnFullName()
+        {
+            String name = firstName + " " + lastName;
+            return name;
+        }
+
+        public Double CalculateGrossPay()
+        {
+            if ((payStatus == "S") || (payStatus == "s"))
+            {
+                return pay;
+            }
+            else
+            {
+                if (hours <= 40)
+                {
+                    return hours * payRate;
+                }
+                else
+                {
+                    Double overtimeHours = hours - 40;
+                    Double straightTime = 40 * payRate;
+                    Double overtimePay = (payRate * 1.5) * overtimeHours;
+                    return straightTime + overtimePay;
+                }
+            }
+        }
+
+        //Calculating Federal Tax
+        public Double CalculateFederal()
+        {
+            return (CalculateGrossPay() * FEDERAL);
+        }
+
+        //Calculating Social Security Tax
+        public Double CalculateSocialSecurity()
+        {
+            return (CalculateGrossPay() * SOCIAL_SECURITY);
+        }
+
+        //Calculating Retirement Contribution
+        public Double CalculateRetirement()
+        {
+            return (CalculateGrossPay() * RETIREMENT);
+        }
+
+        //Calculating Total Deductions 
+        public Double CalculateTotalDeductions()
+        {
+            return (CalculateFederal() + CalculateSocialSecurity() + CalculateRetirement());
+        }
+
+        //Calculateing Net Pay
+        public Double CalculateTakeHomePay()
+        {
+            return (CalculateGrossPay() - CalculateTotalDeductions());
+        }
+
+        public override string ToString()
+        {
+            return "\nWeekly Information For " + ReturnFullName() +
+                "\n\nEmployee ID: " + employeeID +
+                "\nGross Pay: " + CalculateGrossPay().ToString("C") +
+                "\nFederal Taxes: " + CalculateFederal().ToString("C") +
+                "\nSocial Security: " + CalculateSocialSecurity().ToString("C") +
+                "\nRetirement Contribution: " + CalculateRetirement().ToString("C") +
+                "\n\nTotal Deductions: " + CalculateTotalDeductions().ToString("C") +
+                "\nTake home pay: " + CalculateTakeHomePay().ToString("C");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using static System.Console;
+
+namespace TakeHomePay
+{
+    class TestApp
+    {
+        static void Main(string[] args)
+        {
+            //call to opening screen method
+            PrintIntro();
+
+            //create a new employee
+            Employee employee1 = new Employee();
+            //firstName
+            employee1.FirstName = AquireStringValues("employee's first name: ");
+            //lastName
+            employee1.LastName = AquireStringValues("employee's last name: ");
+            //employeeID
+            employee1.EmployeeID = AquireStringValues("the employee's identification number: ");
+            //salary or hourly
+            employee1.PayStatus = AquireStringValues("an S for salaried employee, \nor an H for hourly employee: ");
+            
+            //pay for the period
+            if ((employee1.PayStatus == "S") || (employee1.PayStatus == "s"))
+            {
+                employee1.Pay = AquireDoubleValues("weekly salary: ");
+            }
+            if ((employee1.PayStatus == "H") || (employee1.PayStatus == "h"))
+            {
+                employee1.hours = AquireDoubleValues("hours for the week: ");
+                employee1.PayRate = AquireDoubleValues("hourly pay rate: ");
+            }
+            else
+            {
+                Write("You have entered an incorrect option\n Returning to Main Menu");
+                Clear();
+                PrintIntro();
+            }
+
+            Employee employee2 = new Employee("Alma", "Hitower", "00987", "S", 5000);
+
+            WriteLine(employee1.ToString());
+
+            WriteLine("\t\t\nWeekly Information For {0}\nEmployeeID: {1}\nEmployee Type: Salaried" +
+               "\nGross Pay: {2:c}\nFederal Taxes: {3:c}\nSocial Security: {4:c}\nRetirement Contribution: {5:c}" +
+               "\n\nTotal Deductions: {6:c}\nTake Home Pay: {7:c}", employee2.ReturnFullName(),
+               employee2.EmployeeID, employee2.CalculateGrossPay(), employee2.CalculateFederal(),
+               employee2.CalculateSocialSecurity(), employee2.CalculateRetirement(), employee2.CalculateTotalDeductions(),
+               employee2.CalculateTakeHomePay());
+
+            ReadKey();
+        }
+
+        //Print intro
+        static public void PrintIntro()
+        {
+            Write("\tEMPLOYEE TAKE HOME PAY APPLICATION" +
+                "\nYou will be asked to enter the name of an employee" +
+                "\nand the type of employee (Hourly or Salaried)." +
+                "\n\nCalculations will be preformed to" +
+                "\ndetermine his/her deductions and take home pay.\n\n");
+        }
+
+        //Asking the user for the string values
+        static public String AquireStringValues(String whichString)
+        {
+            Write("Please enter {0}", whichString);
+            String inValue = ReadLine();
+
+            return inValue;
+        }
+
+        //Asking the user for the double values
+        static public Double AquireDoubleValues(String whichString)
+        {
+            Write("Please enter {0}", whichString);
+            String inValue = ReadLine();
+
+            return double.Parse(inValue); ;
+        }
+
+
+    }
+}
